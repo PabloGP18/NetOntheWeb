077717f0326acd5d8114f3cc2adde0afec182392
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-09-22 16:48:25 +0200

    Refactored id of students

--- Pages/Students.cshtml.cs
+++ Pages/Students.cshtml.cs
@@ -8,14 +8,14 @@ namespace Students.Pages
     {
         
         public Student Pablo = new(1,"Pablo",1 );
-        public Student Zeno = new(1,"Zeno",2 );
-        public Student Michael = new(1,"MichaeL",3 );
-        public Student Shiva = new(2,"Shiva",1 );
-        public Student Lucas = new(2,"Lucas",2 );
-        public Student Snehal = new(2,"Snehal",3 );
-        public Student Besart = new(3,"Besart",1 );
-        public Student Mo = new(3,"Mo",2 );
-        public Student Brian = new(3,"Brain",3 );
+        public Student Zeno = new(2,"Zeno",2 );
+        public Student Michael = new(3,"MichaeL",3 );
+        public Student Shiva = new(4,"Shiva",1 );
+        public Student Lucas = new(5,"Lucas",2 );
+        public Student Snehal = new(6,"Snehal",3 );
+        public Student Besart = new(7,"Besart",1 );
+        public Student Mo = new(8,"Mo",2 );
+        public Student Brian = new(9,"Brain",3 );
         public string Message { get; set; } = "In page model: ";
 
         public void OnGet()
