f4cf697490a37e2799ce2c1bf441434b00dac2e1
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-09-22 11:09:12 +0200

    made a model and displayed it in the view

---
+++ Pages/HelloWeb.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HelloWorld.Pages
+{
+    public class HelloWebModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+        public string Message { get; set; } = "In page model: ";
+    }
+}
