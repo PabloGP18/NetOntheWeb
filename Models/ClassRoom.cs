eb12472ecaca6569f53a2b7f833bd3c3ae1f88ea
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-09-22 16:45:37 +0200

    Added a class ClassRooms

---
+++ Models/ClassRoom.cs
@@ -0,0 +1,25 @@
+﻿namespace NetOntheWeb.Models;
+
+public class ClassRoom
+{
+    private int _id;
+    private string _name;
+
+    public ClassRoom(int id, string name)
+    {
+        _id = id;
+        _name = name;
+    }
+
+    public int Id
+    {
+        get => _id;
+        set => _id = value;
+    }
+
+    public string Name
+    {
+        get => _name;
+        set => _name = value ?? throw new ArgumentNullException(nameof(value));
+    }
+}
