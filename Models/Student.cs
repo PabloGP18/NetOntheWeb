836710fc7de82a5eeed495713712cdb9e7a43d6b
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-09-22 14:33:07 +0200

    Made a directory models with a Student class

---
+++ Models/Student.cs
@@ -0,0 +1,34 @@
+﻿namespace NetOntheWeb.Models;
+
+public class Student
+{
+    private int id;
+    private string name;
+    private int classId;
+
+
+    public Student(int id, string name, int classId)
+    {
+        this.id = id;
+        this.name = name;
+        this.classId = classId;
+    }
+
+    public int Id
+    {
+        get => id;
+        set => id = value;
+    }
+
+    public string Name
+    {
+        get => name;
+        set => name = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public int ClassId
+    {
+        get => classId;
+        set => classId = value;
+    }
+}
